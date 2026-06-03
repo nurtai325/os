a6805b98dc5f2f71de72f11ef4b01b50fe3f008d
nurtai325 <nurtolymbek23@gmail.com>
2026-06-04 01:09:43 +0500

    chore: refactoring

--- asm.h
+++ asm.h
@@ -5,6 +5,4 @@ void outb(unsigned short port, unsigned char data);
 
 unsigned char inb(unsigned short port);
 
-void gdt_flush();
-
 #endif
--- asm.s
+++ asm.s
@@ -10,17 +10,3 @@ inb:
     mov dx, [esp + 4]
     in  al, dx
     ret
-
-global gdt_flush
-extern gp
-gdt_flush:
-    lgdt [gp]
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp 0x08:flush2
-flush2:
-    ret
