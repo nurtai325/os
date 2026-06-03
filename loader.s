30b3cf58b0eae117358491f38bf304607514c36d
nurtai325 <nurtolymbek23@gmail.com>
2026-06-04 01:25:30 +0500

    chore: refactoring

--- loader.s
+++ loader.s
@@ -1,6 +1,6 @@
-MAGIC_NUMBER equ 0x1BADB002
-FLAGS        equ 0x0
-CHECKSUM    equ -MAGIC_NUMBER
+MAGIC_NUMBER      equ 0x1BADB002
+FLAGS             equ 0x0
+CHECKSUM          equ -MAGIC_NUMBER
 KERNEL_STACK_SIZE equ 4096
 
 section .bss
@@ -19,7 +19,6 @@ section .text
 
 loader:
     cli
-    mov eax, 0xCAFEBABE
     mov esp, kernel_stack + KERNEL_STACK_SIZE
 
     call kmain
